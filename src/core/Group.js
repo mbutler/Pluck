4ab0d47797b14634dad5a26d333a83f75c35daf7
Matthew Butler <matthewtbutler@gmail.com>
2024-06-17 01:11:58 -0500

    use async/await in Group

--- src/core/Group.js
+++ src/core/Group.js
@@ -1,95 +1,107 @@
-import Sound from './Sound.js';
+import Sound from './Sound.js'
 
-const groupProperties = new WeakMap();
+const groupProperties = new WeakMap()
 
 class Group {
   constructor(sounds = []) {
     if (sounds.length === 0) {
-      throw new Error('Group requires at least one sound');
+      throw new Error('Group requires at least one sound')
     }
 
-    const context = sounds[0].context; // Use the context of the first sound
-    const gainNode = context.createGain();
+    const context = sounds[0].context // Use the context of the first sound
+    const gainNode = context.createGain()
     const properties = {
       context,
       gainNode,
-      sounds,
-    };
+      sounds: [],
+      muted: false,
+    }
 
-    groupProperties.set(this, properties);
+    groupProperties.set(this, properties)
 
     sounds.forEach((sound) => {
       if (sound instanceof Sound) {
-        sound.connect(gainNode);
-        console.log('Sound connected to group gain node:', sound);
+        this.addSound(sound)
       } else {
-        console.error('Sound is not an instance of Sound class:', sound);
+        console.error('Sound is not an instance of Sound class:', sound)
       }
-    });
+    })
 
-    gainNode.connect(context.destination);
+    gainNode.connect(context.destination)
   }
 
   get context() {
-    return groupProperties.get(this).context;
+    return groupProperties.get(this).context
   }
 
   get gainNode() {
-    return groupProperties.get(this).gainNode;
+    return groupProperties.get(this).gainNode
   }
 
   get sounds() {
-    return groupProperties.get(this).sounds;
+    return groupProperties.get(this).sounds
   }
 
-  play() {
-    this.sounds.forEach((sound) => sound.play());
+  async play() {
+    const promises = this.sounds.map(async (sound) => {
+      if (!sound.isPlaying) {
+        try {
+          await sound.play()
+          sound.isPlaying = true
+        } catch (error) {
+          console.error('Error playing sound:', error)
+        }
+      }
+    })
+    await Promise.all(promises)
   }
 
-  stop() {
-    this.sounds.forEach((sound) => sound.stop());
+  async stop() {
+    const promises = this.sounds.map(async (sound) => {
+      if (sound.isPlaying) {
+        sound.stop()
+        sound.isPlaying = false
+      }
+    })
+    await Promise.all(promises)
   }
 
-  pause() {
-    this.sounds.forEach((sound) => sound.pause());
-  }
-
-  addSound(sound) {
+  async addSound(sound) {
     if (!(sound instanceof Sound)) {
-      console.error('The sound is not an instance of Sound class:', sound);
-      return;
+      console.error('The sound is not an instance of Sound class:', sound)
+      return
     }
 
-    const properties = groupProperties.get(this);
-    properties.sounds.push(sound);
-    sound.connect(properties.gainNode);
-    console.log('Added and connected new sound to group gain node:', sound);
+    const properties = groupProperties.get(this)
+    properties.sounds.push(sound)
+    sound.connect(properties.gainNode)
+    console.log('Added and connected new sound to group gain node:', sound)
   }
 
-  removeSound(sound) {
-    const properties = groupProperties.get(this);
-    const index = properties.sounds.indexOf(sound);
+  async removeSound(sound) {
+    const properties = groupProperties.get(this)
+    const index = properties.sounds.indexOf(sound)
     if (index === -1) {
-      console.warn('The sound is not in the group');
-      return;
+      console.warn('The sound is not in the group')
+      return
     }
 
-    sound.disconnect(properties.gainNode);
-    properties.sounds.splice(index, 1);
-    console.log('Removed and disconnected sound from group gain node:', sound);
+    sound.disconnect(properties.gainNode)
+    properties.sounds.splice(index, 1)
+    console.log('Removed and disconnected sound from group gain node:', sound)
   }
 
   set volume(value) {
     if (value < 0 || value > 1) {
-      console.warn('Volume value must be between 0 and 1.');
-      return;
+      console.warn('Volume value must be between 0 and 1.')
+      return
     }
-    groupProperties.get(this).gainNode.gain.value = value;
+    groupProperties.get(this).gainNode.gain.value = value
   }
 
   get volume() {
-    return groupProperties.get(this).gainNode.gain.value;
+    return groupProperties.get(this).gainNode.gain.value
   }
 }
 
-export default Group;
+export default Group
